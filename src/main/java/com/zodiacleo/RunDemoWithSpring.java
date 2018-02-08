980c14b0c9b5090637efd5750e8942c2bb601d58
Nirmit Paresh Dalal <nirmit.leo@gmail.com>
2018-02-08 22:33:03 +0530

    Constructor injection v/s Setter injection Constructor injection should be used when creating immutable objects, and when the object cannot exist without the dependencies (classes which require JDBC connection). In any other scenario, both can be used.
    Example of constructor injection - public BaseballGame(Team home, Team away) {}
    Example of setter injection - public setDataSource(DataSource ds){}

--- src/main/java/com/zodiacleo/RunDemoWithSpring.java
+++ src/main/java/com/zodiacleo/RunDemoWithSpring.java
@@ -1,6 +1,7 @@
 package com.zodiacleo;
 
 import com.zodiacleo.entities.Game;
+import com.zodiacleo.entities.Team;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,7 +12,12 @@ public class RunDemoWithSpring
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Game game = context.getBean("game", Game.class);
         
-        System.out.println(game.playGame());
+        System.out.println("The winner of the game 1 is " + game.playGame());
+        
+        
+        Team royals = context.getBean("royals", Team.class);
+        game.setAwayTeam(royals);
+        System.out.println("The winner of the game 2 is " + game.playGame());
         
         int count = context.getBeanDefinitionCount();
         System.out.println("Total bean count = " + count);
