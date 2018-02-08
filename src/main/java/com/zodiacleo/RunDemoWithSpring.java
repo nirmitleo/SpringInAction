f64505f10a685f57f8caf971b15ad366025f2acb
Nirmit Paresh Dalal <nirmit.leo@gmail.com>
2018-02-08 18:34:18 +0530

    XML based application context.

--- src/main/java/com/zodiacleo/RunDemoWithSpring.java
+++ src/main/java/com/zodiacleo/RunDemoWithSpring.java
@@ -2,13 +2,13 @@ package com.zodiacleo;
 
 import com.zodiacleo.entities.Game;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RunDemoWithSpring
 {
     public static void main(String[] a)
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
         Game game = context.getBean("game", Game.class);
         
         System.out.println(game.playGame());
